315cc21036e371a8e37f52bcb6afc47834ab35bb
Anton Bibergal <nathgilt@gmail.com>
2021-01-02 21:20:50 +0200

    test 0.2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,44 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components/macro";
 import { Link } from "react-router-dom";
 import { menuData } from "../data/MenuData";
 
 const Nav = styled.nav`
   height: 60px;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  z-index: 100;
+  position: fixed;
+  width: 100%;
+  display: flex;
+  background: #000;
+`;
 
-  background: red;
+const NavLink = css`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  text-decoration: none;
 `;
 
 const Logo = styled(Link)`
-  color: #fff;
+  ${NavLink}
+  font-style: italic;
 `;
 
 const MenuBars = styled.i``;
 
-const NavMenu = styled.div``;
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
-const NavMenuLinks = styled(Link)``;
+const NavMenuLinks = styled(Link)`
+  ${NavLink}
+`;
 
 const Navbar = () => {
   return (
@@ -25,13 +46,12 @@ const Navbar = () => {
       <Logo to="/">AVITO</Logo>
       <MenuBars />
       <NavMenu>
-        {menuData.map((item, index) => {
+        {menuData.map((item, index) => (
           <NavMenuLinks to={item.link} key={index}>
             {item.title}
-          </NavMenuLinks>;
-        })}
+          </NavMenuLinks>
+        ))}
       </NavMenu>
-      <h1>Navbar</h1>
     </Nav>
   );
 };
