19eb7c8dd149a396deda3f92ffe7dbe76649698d
Anton Bibergal <nathgilt@gmail.com>
2021-01-02 22:14:46 +0200

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,8 @@ import styled, { css } from "styled-components/macro";
 import { Link } from "react-router-dom";
 import { menuData } from "../data/MenuData";
 import Button from "./Button";
+import Bars from "../images/bars.svg";
+// import { FaBars } from 'react-icons/fa';
 
 const Nav = styled.nav`
   height: 60px;
@@ -30,12 +32,26 @@ const Logo = styled(Link)`
   font-style: italic;
 `;
 
-const MenuBars = styled.i``;
+const MenuBars = styled.i`
+  display: none;
+`;
+
+// const MenuBars = styled(FaBars)`
+//   display: none;
+
+//   @media screen and (max-width: 768px) {
+//       display: block;
+//   }
+// `;
 
 const NavMenu = styled.div`
   display: flex;
   align-items: center;
   margin-right: -48px;
+
+  @media screeen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const NavMenuLinks = styled(Link)`
@@ -46,6 +62,10 @@ const NavBtn = styled.div`
   display: flex;
   align-items: center;
   margin-right: 24px;
+
+  @media screeen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const Navbar = () => {
