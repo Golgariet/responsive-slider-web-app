f94bd83bc5d78e0904ade0b19926aea1235b2a2a
Anton Bibergal <nathgilt@gmail.com>
2021-01-03 19:45:00 +0200

    data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import GlobalStyles from "./GlobalStyles";
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <GlobalStyles />
       <Navbar />
+      <Hero />
     </>
   );
 }
---
+++ src/data/SliderData.js
@@ -0,0 +1,42 @@
+export const SliderData = [
+  {
+    title: "Luxury Villa in Bali, Indonesia",
+    price: "$4,280,000",
+    path: "/home",
+    label: "View Home",
+    image: ImageOne,
+    alt: "House",
+  },
+  {
+    title: "Luxury Villa in Bali, Indonesia",
+    price: "$4,980,000",
+    path: "/home",
+    label: "View Home",
+    image: ImageOne,
+    alt: "House",
+  },
+  {
+    title: "House in San Jose, Costa rica",
+    price: "$2,110,000",
+    path: "/home",
+    label: "View Home",
+    image: ImageOne,
+    alt: "House",
+  },
+  {
+    title: "House in Calabasas, California",
+    price: "$6,780,800",
+    path: "/home",
+    label: "View Home",
+    image: ImageOne,
+    alt: "House",
+  },
+  {
+    title: "House in Houston, Texas",
+    price: "$7,576,000",
+    path: "/home",
+    label: "View Home",
+    image: ImageOne,
+    alt: "House",
+  },
+];
