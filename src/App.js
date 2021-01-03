ce95b80bccf35152c42bb29615efc7f4bf6229dd
Anton Bibergal <nathgilt@gmail.com>
2021-01-03 21:13:35 +0200

    data within ReactJs

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import GlobalStyles from "./GlobalStyles";
+import { SliderData } from "./data/SliderData";
 
 function App() {
   return (
     <>
       <GlobalStyles />
       <Navbar />
-      <Hero />
+      <Hero slides={SliderData} />
     </>
   );
 }
--- src/data/SliderData.js
+++ src/data/SliderData.js
@@ -1,8 +1,13 @@
+import ImageOne from "../images/house-1.jpg";
+import ImageTwo from "../images/house-2.jpg";
+import ImageThree from "../images/house-3.jpg";
+import ImageFour from "../images/house-4.jpg";
+
 export const SliderData = [
   {
     title: "Luxury Villa in Bali, Indonesia",
     price: "$4,280,000",
-    path: "/home",
+    path: "/homes",
     label: "View Home",
     image: ImageOne,
     alt: "House",
@@ -10,15 +15,15 @@ export const SliderData = [
   {
     title: "Luxury Villa in Bali, Indonesia",
     price: "$4,980,000",
-    path: "/home",
+    path: "/homes",
     label: "View Home",
     image: ImageOne,
     alt: "House",
   },
   {
-    title: "House in San Jose, Costa rica",
+    title: "House in San Jose, Costa Rica",
     price: "$2,110,000",
-    path: "/home",
+    path: "/homes",
     label: "View Home",
     image: ImageOne,
     alt: "House",
@@ -26,7 +31,7 @@ export const SliderData = [
   {
     title: "House in Calabasas, California",
     price: "$6,780,800",
-    path: "/home",
+    path: "/homes",
     label: "View Home",
     image: ImageOne,
     alt: "House",
@@ -34,9 +39,52 @@ export const SliderData = [
   {
     title: "House in Houston, Texas",
     price: "$7,576,000",
-    path: "/home",
+    path: "/homes",
     label: "View Home",
     image: ImageOne,
     alt: "House",
   },
 ];
+
+// export const SliderDataTwo = [
+//   {
+//     title: "Luxury Villa in Las Vegas, Nevada",
+//     price: "$8,290,000",
+//     path: "/homes",
+//     label: "View Home",
+//     image: ImageOne,
+//     alt: "House",
+//   },
+//   {
+//     title: "House in Houston, Texas",
+//     price: "$4,980,000",
+//     path: "/homes",
+//     label: "View Home",
+//     image: ImageOne,
+//     alt: "House",
+//   },
+//   {
+//     title: "House in Alajuela, Costa Rica",
+//     price: "$2,110,000",
+//     path: "/homes",
+//     label: "View Home",
+//     image: ImageOne,
+//     alt: "House",
+//   },
+//   {
+//     title: "House in Calabasas, California",
+//     price: "$6,780,800",
+//     path: "/homes",
+//     label: "View Home",
+//     image: ImageOne,
+//     alt: "House",
+//   },
+//   {
+//     title: "House in Boston, Massachussets",
+//     price: "$7,576,000",
+//     path: "/homes",
+//     label: "View Home",
+//     image: ImageOne,
+//     alt: "House",
+//   },
+// ];
