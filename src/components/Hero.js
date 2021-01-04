e2a4d4831d1c7505a24107177d0021b8a7c8f000
Anton Bibergal <nathgilt@gmail.com>
2021-01-04 19:51:18 +0200

    basic layout

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -72,6 +72,20 @@ const HeroContent = styled.div`
   max-width: 1600px;
   width: calc(100% - 100px);
   color: #fff;
+
+  h1 {
+    font-size: clamp(1rem, 8vw, 2rem);
+    font-weight: 400;
+    text-transform: uppercase;
+    text-shadow: 0px 0px 20px rgba(0, 0, 0, 0.4);
+    text-align: left;
+    margin-bottom: 0.8rem;
+  }
+
+  p {
+    margin-bottom: 1.2rem;
+    text-shadow: 0px 0px 20px rgba(0, 0, 0, 0.4);
+  }
 `;
 
 const Arrow = styled(IoMdArrowRoundForward)``;
